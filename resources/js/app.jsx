2f9d396122642a305ff660143e1eb74e88eab64b
Chinedu Chaz <ccognate@gmail.com>
2023-10-31 12:59:04 +0100

    initial setup laravel & reactjs

---
+++ resources/js/app.jsx
@@ -0,0 +1,10 @@
+import './bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './components/Home';
+
+const app = document.getElementById('app-client');
+
+
+ReactDOM.createRoot(app).render(<Home/>);
+
---
+++ resources/js/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+export default  function Home()
+{
+    return (
+        <div className="row justify-content-center">
+        <div className="col-md-8">
+            <div className="card">
+                <div className="card-header">Home</div>
+
+                <div className="card-body">
+                        Page generated with react;
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+}
